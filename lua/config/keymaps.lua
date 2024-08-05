b70983da98682f81cf938b82e4ba7edb00eb033a
aditya-g36 <adityagaur8962@gmail.com>
2024-08-05 18:06:02 +0530

    updated keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,7 +21,8 @@ vim.keymap.set("n", "<leader>h", "<Cmd>wincmd h<CR>", { desc = "Move cursor to l
 vim.keymap.set("n", "<leader>j", "<Cmd>wincmd j<CR>", { desc = "Move cursor to bottomw window" })
 vim.keymap.set("n", "<leader>k", "<Cmd>wincmd k<CR>", { desc = "Move cursor to top window" })
 vim.keymap.set("n", "<leader>l", "<Cmd>wincmd l<CR>", { desc = "Move cursor to right window" })
-vim.keymap.set("n", "<C-S>", "<Cmd>write<CR>", { desc = "Save buffer" })
+vim.keymap.set("n", "<S-h>", "b", { desc = "Move backward by a word" })
+vim.keymap.set("n", "<S-l>", "w", { desc = "Move forward by a word" })
 --split window
 vim.keymap.set("n", "sj", ":split<Return>")
 vim.keymap.set("n", "sl", ":vsplit<Return>")
@@ -29,7 +30,7 @@ vim.keymap.set("n", "sl", ":vsplit<Return>")
 vim.keymap.set("n", "<leader>q", "<Cmd>bprevious<CR>", { desc = "Go to previous buffer" })
 vim.keymap.set("n", "<leader>p", "<Cmd>bnext<CR>", { desc = "Go to next buffer" })
 vim.keymap.set("n", "<leader>w", "<Cmd>bdelete<CR>", { desc = "Delete buffer" })
-
+vim.keymap.set("n", "<C-S>", "<Cmd>write<CR>", { desc = "Save buffer" })
 -- Diagnostics
 vim.keymap.set("n", "<C-j>", function()
   vim.diagnostic.goto_next()
@@ -42,6 +43,11 @@ end)
 vim.keymap.set("n", "<leader>i", function()
   require("craftzdog.lsp").toggleInlayHints()
 end)
+
+-- File tree operations
+vim.keymap.set("n", "d", ":Neotree action=delete<CR>", { desc = "Delete file in file tree" })
+vim.keymap.set("n", "r", ":Neotree action=rename<CR>", { desc = "Rename file in file tree" })
+vim.keymap.set("n", "?", ":Neotree show_help<CR>", { desc = "Neo-tree Help", silent = true })
 
 -- utils.key_mapper('n', '<leader>dn', ':lua vim.diagnostic.goto_next({float={border="rounded"}})<CR>')
 -- utils.key_mapper('n', '<leader>dp', ':lua vim.diagnostic.goto_prev({float={border="rounded"}})<CR>')
